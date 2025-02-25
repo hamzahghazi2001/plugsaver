75f09b21c41bd138fd6560c9125ab14a302a22e5
ZeeshanAhmadHW <za2044@hw.ac.uk>
2025-02-25 16:27:01 +0400

    Day 2 of rewards work
    more to be done soon

--- backend/app/rewards.py
+++ backend/app/rewards.py
@@ -8,30 +8,40 @@ def Points_and_badges(rewards_id):
     try:
         cost_saved = supabase.table('analytics').select('cost_saved').eq('analytics_id', rewards_id).execute()
         energy_saved = supabase.table('analytics').select('energy_saved').eq('analytics_id', rewards_id).execute()
-        points = (0.5 * cost_saved) + (0.5 * energy_saved)
+        points = (0.5 * cost_saved["data"][0]["cost_saved"]) + (0.5 * energy_saved["data"][0]["energy_saved"])
         existing_record = supabase.table('rewards').select('*').eq('rewards_id', rewards_id).execute()
         #updates points if record already exists, adds otherwise
         if existing_record.data:
             supabase.table('rewards').update({'points': points}).eq('rewards_id', rewards_id).execute()
         else:
             supabase.table('rewards').insert({'rewards_id': rewards_id, 'points': points}).execute()
-        #updates badge booleans after each change
-        update_badges(rewards_id, points, energy_saved)
+        #updates badge booleans
+        update_badges(rewards_id, points, energy_saved["data"][0]["energy_saved"])
     except Exception as e:
         print("Error:", e)  
         return 0
     
-#function to update badge booleans
+#function to update badge booleans, called automatically by main function
 def update_badges(rewards_id, points, energy_saved):
     try:
-        #find status of each badge, champion badges kept to false till logic found
+        #find status of saver badges
         bronze_saver = True if energy_saved >= 100 else False
         silver_saver = True if energy_saved >= 1000 else False
         gold_saver = True if energy_saved >= 10000 else False
-        beginner_points = True if points >= 100 else False
-        competent_points = True if points >= 1000 else False
-        professional_points = True if points >= 10000 else False
+        #find status of points badges
+        beginner_points = True if points >= 25 else False
+        competent_points = True if points >= 100 else False
+        professional_points = True if points >= 500 else False
+        #find status of champion badges
         household_champion, local_champion, global_champion = False
+        household_code = supabase.table('users').select('household_code').eq('user_id', rewards_id).execute()
+        household_members = supabase.table('users').select('user_id').eq('household_code', household_code["data"][0]["household_code"]).execute()
+        for x in household_members["data"]:
+            member = supabase.table('rewards').select('points').eq('rewards_id', x["user_id"]).execute()
+            household_champion = True if points > member else False
+        #insert local_champion logic here when found
+        max_global = supabase.table('rewards').select('max(points)').eq('rewards_id', rewards_id).execute()
+        global_champion = True if points > max_global else False
         #update badges with found results
         supabase.table('rewards').update(
         {'bronze_saver_badge': bronze_saver, 
